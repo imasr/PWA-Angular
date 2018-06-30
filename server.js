985ce95ca5ff2a4f116fffc4c01a4de4bba61deb
imasr <ashishs723@gmail.com>
2018-06-30 23:00:30 +0530

    make it better

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/social-login'));
-
-app.get('/*', function (req, res) {
-
-  res.sendFile(path.join(__dirname + '/dist/social-login/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
