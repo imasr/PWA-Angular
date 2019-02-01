faa39fdc26f81e07a6824fc9de8715607e06b0e2
ashish <ashishs723@gmail.com>
2019-02-01 17:26:41 +0530

    dummy cmp

---
+++ src/app/dummy/dummy.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DummyComponent } from './dummy.component';
+
+@NgModule({
+  declarations: [DummyComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DummyModule { }
